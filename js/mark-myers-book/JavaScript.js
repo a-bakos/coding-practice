5af52fecf774842f671d149eb38c287833087991
a-bakos <bakosattila@fastem.com>
2017-02-02 18:48:55 +0000

    JS

---
+++ js/mark-myers-book/JavaScript.js
@@ -0,0 +1,32 @@
+document.getElementById
+document.getElementsByClassName
+document.getElementsByName
+document.getElementsByTagName
+
+/*indexOf
+selectedIndex
+parseFloat
+parseInt
+pop
+push
+shift
+unshift
+splice
+innerHTML*/
+
+// ----------------------------------------------------------------------------
+
+/*
+Finding segments:
+
+  indexOf()
+  The indexOf method finds only the first instance of the segment you're
+  looking for.
+
+  To find the last instance of a segment in a string, use
+  lastIndexOf()
+
+  example:
+*/
+  var firstChar = text.indexOf("Something");
+  // If the segment doesn't exist, the method assigns-1 to the variable
