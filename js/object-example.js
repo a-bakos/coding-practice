0d14fd0765af6f084c2809cb582627d29f87a8d3
a-bakos <bakosattila@fastem.com>
2017-02-08 17:48:55 +0000

    object ex.

---
+++ js/object-example.js
@@ -0,0 +1,26 @@
+// This is an example object
+// Eloquent JS book:
+var day1 = {
+  squirrel: false,
+  events: ["work", "touched tree", "pizza", "running", "television"]
+};
+
+
+
+// Mark Myers book
+var plan1 = {
+  name: "basic", // name -> property (of the object); "basic" -> value (of the object's property)
+  price: 3.99,
+  space: 100,
+  transfer: 1000,
+  pages: 10
+};
+
+// Now let's add one more property:
+plan1.discountMonths = [6, 7, 11];
+
+// This statement assigns the third element of the array to the month variable:
+var month = plan1.discountMonths[2];
+
+// To change the value of an object's property, a simple assignment
+plan1.name = "medium";
