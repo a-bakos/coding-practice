d060337b0e0e1bae27d0f7a8b16754d81e144aa7
Attila Bakos <bakosattila@fastem.com>
2017-05-16 21:25:57 +0100

    ternary operator

---
+++ js/ternary.js
@@ -0,0 +1,17 @@
+// Ternary operator aka conditional operator
+
+// condition ? expr1 : expr2
+
+// Here is an example of code that could be shortened with the conditional operator:
+
+if (iGotTheKey) {
+  iGotTheSecret();
+}
+else {
+  dunnoAnySecret();
+}
+
+// This can be shortened with the ?: like so:
+iGotTheKey ? iGotTheSecret() : dunnoAnySecret();
+
+// if condition is true then expr1, otherwise expr2
