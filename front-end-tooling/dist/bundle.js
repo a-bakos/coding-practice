dd16606517e164ed9adbf57654b323a93d7e8f5a
a-bakos <bakosattila@fastem.com>
2017-10-21 20:28:34 +0100

    Gulp tasks

--- front-end-tooling/dist/bundle.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello from file 1");
-console.log("Hello from file 2");
-var number = 100 + 100;
--- front-end-tooling/gulpfile.js
+++ front-end-tooling/gulpfile.js
@@ -2,13 +2,38 @@
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var del = require('del');
 
-gulp.task('test', function testInternal() {
+gulp.task('hello', function helloInternal() {
 	console.log('Hello gulp');
 });
 
 gulp.task('scripts', function scriptsInternal() {
 	return gulp.src('scripts/**/*.js')
-		.pipe(concat('bundle.js'))
+		.pipe(concat('bundle.min.js'))
+		.pipe(uglify())
 		.pipe(gulp.dest('dist'));
 });
+
+gulp.task('test', function testInternal() {
+	return gulp.src('scripts/**/.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(jshint.reporter('fail'));
+});
+
+gulp.task('clean', function cleanInternal() {
+	return del(['dist']);
+});
+
+gulp.task('default',
+    gulp.series('clean',
+        gulp.parallel(
+        	'hello',
+			'scripts',
+			'test'
+		)
+	)
+);
