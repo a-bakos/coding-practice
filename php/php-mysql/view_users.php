93587461a36f6038f036313a9c1137336f88ad9d
a-bakos <bakosattila@fastem.com>
2018-02-02 19:58:59 +0000

    PHP/MySQL - sending values to a script

--- php/php-mysql/view_users.php
+++ php/php-mysql/view_users.php
@@ -10,7 +10,7 @@
 require ( 'mysqli_connect.php' ); // connect to the db
 
 // make the query
-$q = "SELECT CONCAT(last_name, ', ', first_name) AS name, DATE_FORMAT(registration_date, '%M %d, %Y') AS dr FROM users ORDER BY registration_date ASC";
+$q = "SELECT last_name, first_name, DATE_FORMAT(registration_date, '%M %d, %Y') AS dr, user_id FROM users ORDER BY registration_date ASC";
 $r = @mysqli_query( $dbc, $q ); // run the query
 
 $num = mysqli_num_rows( $r ); // count the number of returned rows
@@ -20,11 +20,23 @@ if ( $num > 0 ) { // if it ran ok
 	echo "<p>There are currently $num registered users.</p>\n";
 
 	echo '<table align="center" cellspacing="3" cellpadding="3" width="75%">
-	<tr><td align="left"><strong>Name</strong></td><td align="left"><strong>Date Registered</strong></td></tr>';
+	<tr>
+		<td align="left"><strong>Edit</strong></td>
+		<td align="left"><strong>Delete</strong></td>
+		<td align="left"><strong>Last name</strong></td>
+		<td align="left"><strong>First name</strong></td>
+		<td align="left"><strong>Date registered</strong></td>
+	</tr>';
 
 	// fetch and print all the records
 	while ( $row = mysqli_fetch_array( $r, MYSQLI_ASSOC ) ) {
-		echo '<tr><td align="left">' . $row['name'] . '</td><td align="left">' . $row['dr'] . '</td></tr>';
+		echo '<tr>
+		<td align="left"><a href="edit_user.php?id=' . $row['user_id'] . '">Edit</a></td>
+		<td align="left"><a href="delete_user.php?id=' . $row['user_id'] . '">Delete</a></td>
+		<td align="left">' . $row['first_name'] . '</td>
+		<td align="left">' . $row['last_name'] . '</td>
+		<td align="left">' . $row['dr'] . '</td>
+		</tr>';
 	}
 
 	echo '</table>';
