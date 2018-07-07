c333bc89c33909c3cf9b2e79165b85eb1858e210
a-bakos <bakosattila@fastem.com>
2018-07-07 11:51:36 +0100

    Python - Write it

---
+++ python/write_it/write_it.py
@@ -0,0 +1,16 @@
+# write it
+# demonstrates writing to a text file
+
+print("Creating a text file with the write() method.")
+text_file = open("write_it.txt", "w")
+
+text_file.write("Line 1\n")
+text_file.write("This is line 2\n")
+text_file.write("Aaand that makes this line 3\n")
+
+text_file.close()
+
+print("\nReading the newly created file.")
+text_file = open("write_it.txt", "r")
+print(text_file.read())
+text_file.close()
