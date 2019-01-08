60096b4deaa33d8474e1cbceba89df9d99be0641
a-bakos <bakosattila@fastem.com>
2019-01-08 19:17:32 +0000

    Add logic

--- js/electron/util-app/renderer.js
+++ js/electron/util-app/renderer.js
@@ -1,3 +1,31 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+// const {app} = require('electron')
+
+function inFocusSound(sound) {
+	const soundToggle = document.getElementById( 'sound-toggle--' + sound )
+	const soundPath = new Audio(__dirname + '/sounds/infocus-' + sound + '.ogg')
+
+	soundToggle.addEventListener('click', () => {
+		if ( soundToggle.parentElement.classList.contains( 'sound-selector__sound--active' ) ) {
+			soundToggle.parentElement.classList.remove( 'sound-selector__sound--active' )
+			soundPath.pause()
+		} else {
+			soundToggle.parentElement.classList.add( 'sound-selector__sound--active' )
+			soundPath.play();
+		}
+	})
+}
+
+inFocusSound('cafe')
+inFocusSound('garden')
+inFocusSound('night')
+inFocusSound('fire')
+inFocusSound('city')
+inFocusSound('snow')
+inFocusSound('water')
+inFocusSound('dawn')
+inFocusSound('pond')
+inFocusSound('wind')
