7251197cc063b08a6d270dff741bf4e8058d2352
a-bakos <bakosattila@fastem.com>
2019-01-08 19:38:37 +0000

    Confirm exit

--- js/electron/util-app/renderer.js
+++ js/electron/util-app/renderer.js
@@ -2,10 +2,21 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const {app} = require('electron').remote
+const {dialog, app} = require('electron').remote
 
 document.getElementById('app-control--exit').addEventListener('click', () => {
-	app.quit()
+	// If any sound is playing, show a dialog box to confirm exit
+	if ( document.querySelector('.sound-selector__sound--active') ) {
+		dialog.showMessageBox({
+			message: 'Are you sure you want to quit?',
+			buttons: ['Yes', 'No']
+		}, (buttonIndex) => {
+			if ( buttonIndex == 0 ) app.quit()
+		})
+	} else {
+		app.quit()
+	}
+
 })
 
 function inFocusSound(sound) {
