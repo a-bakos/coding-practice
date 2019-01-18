a7179b4e17f91a14f2c5ddf9cd03cc300b289bb4
a-bakos <bakosattila@fastem.com>
2019-01-18 19:19:54 +0000

    Move Infocus desktop app out into its own repo

--- js/electron/util-app/help.js
+++
@@ -1,6 +0,0 @@
-const {app, BrowserWindow} = require('electron').remote
-
-document.getElementById('app-control--exit').addEventListener('click', () => {
-	// Change this to close only the child window instance
-	app.quit()
-})
