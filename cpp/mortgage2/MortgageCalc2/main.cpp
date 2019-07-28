ee61d8ba701ca6e91af7bb27f16db361a0dbec88
a-bakos <bakosattila@fastem.com>
2019-07-28 11:13:33 +0100

    Learning c++

--- cpp/mortgage2/MortgageCalc2/main.cpp
+++ cpp/mortgage2/MortgageCalc2/main.cpp
@@ -12,6 +12,12 @@ int main()
 	const unsigned short MONTHS_IN_YEARS = 12;
 	bool approved = true; // Approved for a loan
 
+	// Establish comparison values as constants.
+	const unsigned short LOW_MAX = 500;
+	const unsigned short MODERATE_MAX = 1500;
+	const unsigned short PRICEY_MAX = 3000;
+	const unsigned short EXPENSIVE_MAX = 10000;
+
 	// Perform basic calculations.
 	float monthly_interest_rate = annual_interest_rate / MONTHS_IN_YEARS;
 	monthly_interest_rate = monthly_interest_rate / 100; // Monthly rate as decimal.
@@ -34,11 +40,29 @@ int main()
 	// Print the monthly payment.
 	cout << monthly_payment << ".\n\n";
 
+	// How expensive is the monthly payment?
+	if (monthly_payment < LOW_MAX) {
+		cout << "Your mortgage payment is a steal!\n\n";
+	} else if (monthly_payment < MODERATE_MAX) {
+		cout << "Your mortgage payment seems reasonable\n\n";
+	} else if (monthly_payment < PRICEY_MAX) {
+		cout << "The budget is getting a little tight.\n\n";
+	} else if (monthly_payment < EXPENSIVE_MAX) {
+		cout << "Ouch!\n\n";
+	} else {
+		cout << "I hope your name is Kiyosaki\n\n";
+	}
+
 	if (approved) {
 		cout << "According to our information, you are already approved for this loan.\n";
 	} else {
 		cout << "According to our information, you are not already approved for this loan\n";
 	}
+
+	// As a ternary statement:
+	cout << "According to our information, you ";
+	cout << ((approved) ?  "are" : "are not");
+	cout << " already approved for this loan.\n";
 
 	// Let the reader know what to do next.
 	cout << "Press enter to continue.\n";
